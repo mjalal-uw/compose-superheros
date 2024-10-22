02d880315cfd160a807f7e5416f164e0a63e5ad4
mehrozejalal <mjalal@uwaterloo.ca>
2024-10-22 15:31:08 -0400

    Update MainActivity to add the TopBar and access the HeroesScreen

--- app/src/main/java/com/example/superheros/MainActivity.kt
+++ app/src/main/java/com/example/superheros/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.example.superheros
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.superheros.model.HeroRepository
 import com.example.superheros.ui.theme.SuperherosTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,9 +25,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SuperherosTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = { TopAppBar() }
+                ) { innerPadding ->
+                    HeroesScreen().HeroesList(
+                        heroes = HeroRepository().loadHeroes(),
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -30,18 +39,36 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun TopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = stringResource(R.string.app_name),
+                style = MaterialTheme.typography.displayLarge
+            )
+        },
         modifier = modifier
     )
 }
 
-@Preview(showBackground = true)
+@Preview("Light Theme", showBackground = true)
+@Preview("Dark Theme", uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
-fun GreetingPreview() {
+fun TopAppBarPreview() {
     SuperherosTheme {
-        Greeting("Android")
+        TopAppBar()
+    }
+}
+
+@Preview(name = "Light Theme", showBackground = true)
+@Preview("Dark Theme", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun HeroesAppPreview() {
+    SuperherosTheme {
+        HeroesScreen().HeroesList(
+            heroes = HeroRepository().loadHeroes()
+        )
     }
 }
